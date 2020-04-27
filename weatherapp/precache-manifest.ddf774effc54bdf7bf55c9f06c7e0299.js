0044d2a69f93a7a9fce2d1cfb56f468ccc96dd0b
Lundorf <baatcher89@gmail.com>
2020-04-27 10:20:15 +0200

    auto-commit

---
+++ weatherapp/precache-manifest.ddf774effc54bdf7bf55c9f06c7e0299.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40859513232913643cc4d283558ab709",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "1ad9968a72cfbae62ceb",
+    "url": "/weatherapp/static/css/2.dd12a9f3.chunk.css"
+  },
+  {
+    "revision": "9db185cbd0a6dea01305",
+    "url": "/weatherapp/static/css/main.2703ca43.chunk.css"
+  },
+  {
+    "revision": "1ad9968a72cfbae62ceb",
+    "url": "/weatherapp/static/js/2.4657ae63.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/weatherapp/static/js/2.4657ae63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db185cbd0a6dea01305",
+    "url": "/weatherapp/static/js/main.9e97c9d4.chunk.js"
+  },
+  {
+    "revision": "28bae3f88fffa69fe8aa",
+    "url": "/weatherapp/static/js/runtime-main.87f5383a.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
